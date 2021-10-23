186963d9c471953bdeb36a2f29206e1fcd3be277
lykismekk <lykismekk@outlook.com>
2021-10-23 11:18:01 +0800

    修改配置文件获取路径

---
+++ src/main/java/com/springselfcoding/StartApplication.java
@@ -0,0 +1,9 @@
+package com.springselfcoding;
+
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+public class StartApplication {
+    public static void main(String[] args) {
+
+    }
+}
